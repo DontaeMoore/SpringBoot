8a2b21e8a01a2f078d610adf9fde6cf14ebd432b
Dontae Moore <dontae_moore132@mymail.eku.edu>
2022-09-22 15:29:14 -0400

    pom changes

--- src/test/java/com/TB_Challenge/TBChallengeApplicationTests.java
+++ src/test/java/com/TB_Challenge/TBChallengeApplicationTests.java
@@ -1,13 +1,8 @@
 package com.TB_Challenge;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class TBChallengeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
