f64d878550ae96b821909582c09ef2e9c5899a70
Dontae Moore <dontae_moore132@mymail.eku.edu>
2022-09-22 11:33:58 -0400

    random changes

--- src/main/java/com/TB_Challenge/TB_ChallengeApplication.java
+++ src/main/java/com/TB_Challenge/TB_ChallengeApplication.java
@@ -2,14 +2,21 @@ package com.TB_Challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TB_ChallengeApplication {
+public class TB_ChallengeApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 
 		System.out.println("http://localhost:8080/");
 		SpringApplication.run(TB_ChallengeApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(TB_ChallengeApplication.class);
 	}
 
 }
--- src/test/java/com/TB_Challenge/TBChallengeApplicationTests.java
+++ src/test/java/com/TB_Challenge/TBChallengeApplicationTests.java
@@ -1,7 +1,8 @@
 package com.TB_Challenge;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class TBChallengeApplicationTests {
